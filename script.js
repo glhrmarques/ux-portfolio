2b7b0fc00d0efbc1080549e624ac02cce4568a03
glhrmarques <gmarques.pd@gmail.com>
2025-11-27 00:44:32 -0300

    Update portfolio layout and add project cards
    - Modified the main title and subtitle in index.html to reflect the portfolio owner.
    - Adjusted the layout to include a gap between elements and changed the minimum height.
    - Implemented a dynamic project card generation in script.js, displaying project names and types.
    - Added new project images for "Beauty Color", "Sales App", and "Marketplace".

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-const elements = {
-    nav: document.querySelector("#nav"),
-};
+const projects = [
+    {name: "Beauty Color", type: "AI Deployment", image: "images/bg-beautycolor.png"},
+    {name: "Sales App", type: "AI Deployment", image: "images/bg-app.png"},
+    {name: "Marketplace", type: "UX", image: "images/bg-marketplace.png"},
+];
 
-const flags = {
-    nav: true,
-};
+const projectsContainer = document.querySelector("#projects-container");
 
-function hideElement() {
-    for (let featureName in elements) {
-        const feature = elements[featureName];
-        const featureBoolean = flags[featureName];
+projects.forEach((project) => {
+    const card = document.createElement("div");
+    card.className = "flex flex-col justify-center items-center flex-1";
 
-        if(feature) {
-            feature.style.display = featureBoolean ? "none" : "block";
-        }
+    const image = document.createElement("img");
+    image.src = project.image;
+    image.alt = project.name;
+    image.className = "w-full aspect-[4/5] object-cover mb-4";
 
-    }
-}
+    const textWrapper = document.createElement("div");
+    textWrapper.className = "flex flex-row justify-between w-full px-4";
 
-hideElement()
+    const title = document.createElement("p");
+    title.className = "project-title text-gray-900 text-lg";
+    title.textContent = project.name;
+
+    const type = document.createElement("p");
+    type.className = "skill-type text-gray-900 text-lg";
+    type.textContent = project.type;
+    
+
+    textWrapper.appendChild(title);
+    textWrapper.appendChild(type);
+
+    card.appendChild(image);
+    card.appendChild(textWrapper);
+    
+    projectsContainer.appendChild(card)
+})
