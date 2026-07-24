5de254d8c139ba4655111fe283f8c2368115a1c4
glhrmarques <gmarques.pd@gmail.com>
2026-07-24 11:54:04 -0300

    button size

--- src/pages/dsPages/Button.jsx
+++ src/pages/dsPages/Button.jsx
@@ -3,8 +3,8 @@ export default function Button() {
       <div className="flex flex-col gap-6">
         <h1 className="text-white text-[40px] font-medium">Button</h1>
 
-        <div className="flex flex-row gap-4 w-full pb-[56px]">
-            <div className="flex flex-col w-full gap-7">
+        <div className="grid grid-cols-2 gap-6 w-full">
+            <div className="flex flex-col w-full gap-6">
                 <div className="h-[300px] bg-[#0E0E0E] p-4 flex flex-col justify-center items-center">
                     <button
                     type="button"
@@ -13,15 +13,12 @@ export default function Button() {
                     Primary
                     </button>
                 </div>
-                <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-4">
                     <p className="text-white font-semibold text-[18px]">Primary</p>
                     <p className="text-white font-regular text-[16px]">The most important action to move forward in a flow, acknowledge and dismiss, or finish a task.</p>
                 </div>
             </div>
-        </div>
-
-        <div className="flex flex-row gap-4 w-full">
-            <div className="flex flex-col w-full gap-7">
+            <div className="flex flex-col w-full gap-6">
                 <div className="h-[300px] bg-[#0E0E0E] p-4 flex flex-col justify-center items-center">
                     <button
                     type="button"
@@ -31,12 +28,12 @@ export default function Button() {
                     </button>
                     
                 </div>
-                <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-4">
                     <p className="text-white font-semibold text-[18px]">Secondary</p>
-                    <p className="text-white font-regular text-[16px]">Use secondary for providing alternatives to the primary action, or when none of your actions are more important than the others.</p>
+                    <p className="text-white font-regular text-[16px]">Supports the primary action by providing an alternative action with lower emphasis.</p>
                 </div>
             </div>
-            <div className="flex flex-col w-full gap-7">
+            <div className="flex flex-col w-full gap-6">
                 <div className="h-[300px] bg-[#0E0E0E] p-4 flex flex-col justify-center items-center">
                     <button
                     type="button"
@@ -45,63 +42,65 @@ export default function Button() {
                     Tertiary
                     </button>
                 </div>
-                <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-4">
                     <p className="text-white font-semibold text-[18px]">Tertiary</p>
-                    <p className="text-white font-regular text-[16px]">Dismissive actions give users a way out of something, letting them cancel, do nothing, dismiss, or skip.</p>
+                    <p className="text-white font-regular text-[16px]">Used for optional or low-priority actions that should remain available without competing for attention.</p>
+                </div>
+            </div>
+            <div className="flex flex-col w-full gap-6">
+                <div className="h-[300px] bg-[#0E0E0E] p-4 flex flex-col justify-center items-center">
+                    <button
+                    type="button"
+                    className="w-fit rounded-full bg-[#E1E9EF] px-12 py-6 text-[16px] font-semibold text-[#777A7C]"
+                    >
+                    Disabled
+                    </button>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <p className="text-white font-semibold text-[18px]">Disabled</p>
+                    <p className="text-white font-regular text-[16px]">Indicates an action that is currently unavailable because the required conditions have not been met.</p>
                 </div>
             </div>
         </div>
 
         <p className="font-['Inter',sans-serif] text-[24px] text-white font-medium pt-[56px]">Size</p>
 
-
-        <div className="flex flex-col gap-6 w-full">
-          <div className="grid grid-cols-3 gap-6 items-center">
-            <p className="font-['Inter',sans-serif] text-[16px] text-white font-regular ">Large</p>
-            <p className="font-['Inter',sans-serif] text-[16px] text-white font-regular">Primary</p>
-            <div className="flex flex-1 justify-center">
-                <button
+        <div className="flex flex-row gap-6 w-full">
+            <div className="flex flex-col w-full gap-6">
+                <div className="h-[300px] bg-[#0E0E0E] p-4 flex flex-col justify-center items-center">
+                    <button
                     type="button"
                     className="w-fit rounded-full bg-[#005BE2] px-12 py-6 text-[16px] font-semibold text-white hover:bg-[#003788] cursor-pointer"
                     >
-                    Primary
-                </button>
+                    Large
+                    </button>
+                </div>
+                <p className="text-white font-semibold text-[18px]">Large</p>
             </div>
-            </div>
-        </div>
-
-        <hr className="border-t border-[#3d3d3d]" />
-
-        <div className="flex flex-col gap-6 w-full">
-          <div className="grid grid-cols-3 gap-6 items-center">
-            <p className="font-['Inter',sans-serif] text-[16px] text-white font-regular ">Medium</p>
-            <p className="font-['Inter',sans-serif] text-[16px] text-white font-regular">Primary</p>
-            <div className="flex flex-1 justify-center">
-                <button
+            <div className="flex flex-col w-full gap-6">
+                <div className="h-[300px] bg-[#0E0E0E] p-4 flex flex-col justify-center items-center">
+                    <button
                     type="button"
-                    className="w-fit rounded-full bg-[#005BE2] px-10 py-4 text-[14px] font-semibold text-white hover:bg-[#003788] cursor-pointer"
+                    className="w-fit rounded-full bg-[#005BE2] px-10 py-4 text-[16px] font-semibold text-white hover:bg-[#003788] cursor-pointer"
                     >
-                    Primary
-                </button>
+                    Medium
+                    </button>
+                </div>
+                <p className="text-white font-semibold text-[16px]">Medium</p>
             </div>
-            </div>
-        </div>
-
-        <hr className="border-t border-[#3d3d3d]" />
-
-        <div className="flex flex-col gap-6 w-full">
-          <div className="grid grid-cols-3 gap-6 items-center">
-            <p className="font-['Inter',sans-serif] text-[16px] text-white font-regular ">Small</p>
-            <p className="font-['Inter',sans-serif] text-[16px] text-white font-regular">Primary</p>
-            <div className="flex flex-1 justify-center">
-                <button
+            <div className="flex flex-col w-full gap-6">
+                <div className="h-[300px] bg-[#0E0E0E] p-4 flex flex-col justify-center items-center">
+                    <button
                     type="button"
                     className="w-fit rounded-full bg-[#005BE2] px-8 py-2 text-[12px] font-semibold text-white hover:bg-[#003788] cursor-pointer"
                     >
-                    Primary
-                </button>
+                    Small
+                    </button>
+                </div>
+                <p className="text-white font-semibold text-[18px]">Small</p>
             </div>
-            </div>
+            
+
         </div>
 
       </div>
