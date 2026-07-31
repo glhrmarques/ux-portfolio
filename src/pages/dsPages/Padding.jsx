2b7b246a6e601d1ae32ecfd55d361a27039beb48
glhrmarques <gmarques.pd@gmail.com>
2026-07-31 14:24:33 -0300

    fixed: text to english

--- src/pages/dsPages/Padding.jsx
+++ src/pages/dsPages/Padding.jsx
@@ -5,7 +5,7 @@ export default function Padding() {
 
       <div className="flex flex-col gap-6 w-full pb-[56px]">
         <div className="grid grid-cols-2 gap-6 items-center">
-          <p className="font-['Inter',sans-serif] text-[24px] text-white font-medium">O que é</p>
+          <p className="font-['Inter',sans-serif] text-[24px] text-white font-medium">What is</p>
           <p className="font-['Inter',sans-serif] text-[16px] text-white font-regular">Preenchimento é o espaçamento interno dos componentes que separa o conteúdo de seus limites externos.</p>
         </div>
       </div>
