291ab3b5ecdb3a6ef46755371588edfa4577982e
glhrmarques <gmarques.pd@gmail.com>
2026-08-03 20:30:13 -0300

    states

--- src/pages/dsPages/InputText.jsx
+++ src/pages/dsPages/InputText.jsx
@@ -7,7 +7,7 @@ export default function InputText() {
                 <div className="flex flex-col w-full gap-6">
                     <div className="h-[300px] bg-[#ffffff] px-[200px] flex flex-col justify-center items-center rounded-[16px]">
                         <form className="flex flex-col gap-2 w-full">
-                            <label className="px-3 text-[14px] font-semibold">
+                            <label className="px-3 text-[14px] font-semibold text-black">
                                 Label
                             </label>
                             <input type="text" placeholder="Placeholder" 
@@ -33,7 +33,7 @@ export default function InputText() {
                 <div className="flex flex-col w-full gap-6">
                     <div className="h-[300px] bg-[#ffffff] p-6 flex flex-col justify-center items-center rounded-[16px]">
                         <form className="flex flex-col gap-2 w-full">
-                            <label className="px-3 text-[14px] font-semibold">
+                            <label className="px-3 text-[14px] font-semibold text-black">
                                 Label
                             </label>
                                 <input type="text" placeholder="Placeholder" value="email@123" 
@@ -61,16 +61,15 @@ export default function InputText() {
                 <div className="flex flex-col w-full gap-6">
                     <div className="h-[300px] bg-[#ffffff] p-6 flex flex-col justify-center items-center rounded-[16px]">
                         <form className="flex flex-col gap-2 w-full">
-                            <label className="px-3 text-[14px] font-semibold">
+                            <label className="px-3 text-[14px] font-semibold text-[#777A7C]">
                                 Label
                             </label>
-                                <input type="text" placeholder="Placeholder" 
+                                <input type="text"  disabled placeholder="email@company.com"
                                     className="
                                     border-2 border-[#E1E9EF] rounded-[12px] p-3
-                                    hover:border-[#000000] cursor-pointer transition-colors
-                                    focus:border-[#000000] focus:outline-[#000000]
-                                    not-placeholder-shown:border-[#000000]
+                                    disabled:bg-[#E1E9EF]/30
                                     text-[16px]
+                                    disabled:cursor-not-allowed
                                     ">
                                 </input>
                         </form>
