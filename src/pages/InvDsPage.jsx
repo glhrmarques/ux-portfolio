1de217b38f862dba26d532ad1576f423b55846f8
glhrmarques <gmarques.pd@gmail.com>
2026-08-03 19:50:34 -0300

    remove inputText

--- src/pages/InvDsPage.jsx
+++ src/pages/InvDsPage.jsx
@@ -10,7 +10,6 @@ import Typography from './dsPages/Typography'
 import Button from './dsPages/Button'
 import ClientCard from './dsPages/ClientCard'
 import Widget from './dsPages/Widget'
-import InputText from './dsPages/InputText'
 
 const menuItems = [
   {
@@ -55,11 +54,6 @@ const menuComponent = [
     id: 'widget',
     label: 'Widget',
     component: Widget,
-  },
-  {
-    id: 'inputText',
-    label: 'Input Text',
-    component: InputText,
   },
 ]
 
