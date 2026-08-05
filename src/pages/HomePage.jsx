3010861ba62f243ea75298695ed6a8baf15d4c9b
glhrmarques <gmarques.pd@gmail.com>
2026-08-05 18:55:15 -0300

    animtions

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -37,7 +37,7 @@ export default function HomePage() {
           />
         </motion.div>
 
-        <div className="flex flex-col gap-2"> 
+        <div className="flex flex-col gap-3"> 
           <motion.div 
               className="flex fle-row gap-3 items-center"
               initial={{ opacity: 0, y: 20 }}
@@ -52,7 +52,7 @@ export default function HomePage() {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 1 }}
           transition={{ duration: 0.6, ease: "easeOut", delay: 0.4 }}
-          className="text-[16px] text-black/50">
+          className="text-[14px] text-black/60">
             Projetando com excelência. Desenvolvendo com propósito.
           </motion.p>
         </div>
@@ -103,27 +103,47 @@ export default function HomePage() {
       transition={{ duration: 0.6, ease: "easeOut", delay: 1.5 }} 
       className="grid grid-cols-3 gap-3">
         {/* First image */}
-        <Link to="/projects/inv-ds" className="cursor-label-target flex flex-col gap-4">
-        <span>App de vendedores</span>
+        <Link to="/projects/rep-sales-app" className="cursor-label-target flex flex-col gap-4 group">
+          <div className="flex flex-row justify-between">
+            <p className="text-[16px] text-black leading-none font-regular">App de Vendas</p>
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              fill="none" viewBox="0 0 24 24" 
+              stroke-width="1.5" stroke="currentColor" 
+              className="size-6 opacity-0 group-hover:opacity-100 group-hover:scale-110 transition-all duration-600">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+            </svg>
+          </div>
+
           <div className="
-            flex items-center justify-center h-[540px] w-full p-6
+            flex items-center justify-center h-[540px] w-full p-10
             bg-[url('/images/cover-background-1.png')] overflow-hidden
-            bg-no-repeat bg-cover bg-center group
+            bg-no-repeat bg-cover bg-center relative
             ">
             <img 
               src="/images/cover-projects-1.png"
               alt="Rep Sales App" 
-              className="h-full w-auto object-contain group-hover:scale-105 transition-transform duration-400" 
+              className="h-full w-auto object-contain group-hover:scale-105 transition-transform duration-600" 
             />
+            <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
           </div>
         </Link>
 
-        <Link to="/projects/inv-ds" className="cursor-label-target flex flex-col gap-4">
-        <span>App de vendedores</span>
+        <Link to="/projects/seller-platform" className="cursor-label-target flex flex-col gap-4 group">
+          <div className="flex flex-row justify-between">
+            <p className="text-[16px] text-black leading-none font-regular">Rotas para Consultores</p>
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              fill="none" viewBox="0 0 24 24" 
+              stroke-width="1.5" stroke="currentColor" 
+              className="size-6 opacity-0 group-hover:opacity-100 group-hover:scale-110 transition-all duration-600">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+            </svg>
+          </div>
           <div className="
-            flex items-center justify-center h-[540px] w-full p-6
+            flex items-center justify-center h-[540px] w-full p-10
             bg-[url('/images/cover-background-2.png')] overflow-hidden
-            bg-no-repeat bg-cover bg-center group
+            bg-no-repeat bg-cover bg-center relative
             ">
             <video 
               src="/images/Screen Recording 2026-02-07 at 00.03.45.mp4" 
@@ -131,23 +151,36 @@ export default function HomePage() {
               loop 
               muted 
               playsInline 
-              className="w-full h-auto object-contain group-hover:scale-105 transition-transform duration-400" 
+              className="w-full h-full object-contain group-hover:scale-105 transition-transform duration-600" 
             />
+            <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+
           </div>
         </Link>
 
-        <Link to="/projects/inv-ds" className="cursor-label-target flex flex-col gap-4">
-        <span>App de vendedores</span>
+        <Link to="/projects/inv-ds" className="cursor-label-target flex flex-col gap-4 group">
+          <div className="flex flex-row justify-between">
+            <p className="text-[16px] text-black leading-none font-regular">Design System</p>
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              fill="none" viewBox="0 0 24 24" 
+              stroke-width="1.5" stroke="currentColor" 
+              className="size-6 opacity-0 group-hover:opacity-100 group-hover:scale-110 transition-all duration-600">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+            </svg>
+          </div>
           <div className="
-            flex items-center justify-center h-[540px] w-full p-6
+            flex items-center justify-center h-[540px] w-full p-10
             bg-[url('/images/cover-background-3.png')] overflow-hidden
-            bg-no-repeat bg-cover bg-center group
+            bg-no-repeat bg-cover bg-center relative
             ">
             <img 
               src="/images/cover-projects-2.png"
               alt="Rep Sales App" 
-              className="h-full w-auto object-contain group-hover:scale-105 transition-transform duration-400" 
+              className="h-full w-auto object-contain group-hover:scale-105 transition-transform duration-600" 
             />
+            <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+
           </div>
         </Link>
       </motion.section>
