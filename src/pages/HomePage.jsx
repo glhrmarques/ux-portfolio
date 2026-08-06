a81f1248431438a46997637114bf6b7b53051b22
glhrmarques <gmarques.pd@gmail.com>
2026-08-06 12:29:28 -0300

    fix padding x

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -22,7 +22,7 @@ export default function HomePage() {
   
 
   return (
-    <main className="mx-auto w-full max-w-[1440px] px-4">
+    <main className="mx-auto w-full max-w-screen-2xl px-4 lg:px-10">
       <CustomCursor />
 
       {/* Hero */}
