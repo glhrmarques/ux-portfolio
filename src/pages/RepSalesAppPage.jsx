5ca72e7f3f0602428b674680ecf0dd7087427b46
glhrmarques <gmarques.pd@gmail.com>
2026-08-11 18:27:43 -0300

    fixed text

--- src/pages/RepSalesAppPage.jsx
+++ src/pages/RepSalesAppPage.jsx
@@ -41,7 +41,7 @@ export default function RepSalesAppPage() {
         >
           <BackArrow />
           <div className="flex flex-col gap-6 sm:gap-10">
-            <h1 className="text-[32px] font-regular leading-none sm:text-[40px]">teste App de Vendedores</h1>
+            <h1 className="text-[32px] font-regular leading-none sm:text-[40px]">App de Vendedores</h1>
             <p className="text-[16px] lg:text-[22px] font-[300]">A Inventa é uma empresa full-service especializada no mercado B2B. Responsável por toda a operação logística, incluindo armazenagem e distribuição, e conta com uma equipe de consultores comerciais por São Paulo.</p>
           </div>
 
