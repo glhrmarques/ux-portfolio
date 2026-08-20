ebed4f2b0626490ebf88ba2e3ed2c5ec2b48992a
glhrmarques <gmarques.pd@gmail.com>
2026-08-20 15:59:08 -0300

    product added state

--- src/pages/dsPages/ProductCard.jsx
+++ src/pages/dsPages/ProductCard.jsx
@@ -77,13 +77,25 @@ export default function ProductCard() {
                         <div className="w-[180px] flex flex-col gap-2">
                             <div className=" flex justify-end items-end p-2
                             bg-[url('/images/product-card-image.png')] bg-no-repeate bg-no-repeat bg-cover bg-center w-full h-[180px]">
-                                <button className=" flex justify-center items-center
-                                w-[40px] h-[40px] bg-[#005BE2] hover:bg-[#003788] rounded-full cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
-                                    class="size-6 text-white">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+
+                                <div className="flex flex-row justify-center gap-2 bg-[#ffffff] border-2 border-[#E1E9EF] rounded-[12px]">
+                                    <button className=" flex justify-center items-center
+                                    min-w-[40px] min-h-[40px] cursor-pointer">
+                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
+                                      class="size-6 text-black">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                     </svg>
-                                </button>
+
+                                    </button>
+                                    <span className="min-w-[40px] min-h-[40px] flex items-center justify-center leading-none">1</span>
+                                    <button className=" flex justify-center items-center
+                                    min-w-[40px] min-h-[40px] cursor-pointer">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
+                                        class="size-6 text-black">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                        </svg>
+                                    </button>
+                                </div>
                             </div>
                             <p className="text-[16px] font-[500] text-black line-clamp-2">Rexona Men Antitranspirante Aerossol Invisible 250ml</p>
                             <div className="flex flex-col gap-3">
@@ -98,8 +110,8 @@ export default function ProductCard() {
                         </div>
                     </div>
                     <div className="flex flex-col gap-4">
-                        <p className="text-white font-semibold text-[18px]">Home widget</p>
-                        <p className="text-white font-regular text-[16px]">Focused to display the main workflows in the app.</p>
+                        <p className="text-white font-semibold text-[18px]">Product added</p>
+                        <p className="text-white font-regular text-[16px]">Allows the user to add and remove products from the shopping cart</p>
                     </div>
                 </div>
 
