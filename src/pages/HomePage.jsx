5f6f88e46760660d320710c84e25385d59c750b7
glhrmarques <gmarques.pd@gmail.com>
2026-09-25 12:40:19 -0300

    profile pic

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -36,7 +36,7 @@ export default function HomePage() {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, ease: "easeOut" }}
-            className="w-[96px] h-[96px]">
+            className="w-[120px] h-[120px]">
               <img 
                 src="/images/myself-01.png" 
                 alt="Guilherme Marques" 
